d7e6b50c9d03ce710debec6165d6c672f3725825
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-27 14:59:46 -0400

    Fail on error

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -8,4 +8,4 @@ bundle install --path Vendor --quiet
 # Hack while we get our PR merged in Danger's repo
 find Vendor/ruby -type d -name ci_source -exec cp Vendor/danger_buddybuild.rb {} \;
 
-bundle exec danger
+bundle exec danger --fail-on-errors=true
