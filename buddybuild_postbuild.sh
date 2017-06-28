d116828f4b5488584c8f76da5a1a71853d1ce8a9
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-28 14:55:31 -0400

    Update config file

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p buddybuild_artifacts/swiftlint
-swiftlint > buddybuild_artifacts/swiftlint/report.xml
+
+swiftlint --config "$BUDDYBUILD_WORKSPACE/.swiftlint.yml" > buddybuild_artifacts/swiftlint/report.xml
