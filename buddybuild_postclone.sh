aabd9286ce2de5a6093cf39ef9c296ad4a7e7645
Romain Pouclet <romain.pouclet@gmail.com>
2018-01-31 14:15:56 -0500

    Remove postclone

--- buddybuild_postclone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-AWS_FOLDER="$HOME/.aws"
-
-mkdir -p $AWS_FOLDER || true
-
-cp "$BUDDYBUILD_SECURE_FILES/aws-config" "$AWS_FOLDER/config"
-cp "$BUDDYBUILD_SECURE_FILES/aws-credentials" "$AWS_FOLDER/credentials"
